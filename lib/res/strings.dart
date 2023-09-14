435eb49c7166fa1a95f4200e9a21ad1ebc367536
Yasser Elbana <74818740+yasser1248@users.noreply.github.com>
2023-09-14 14:49:02 +0300

    matchday

--- lib/res/strings.dart
+++
@@ -1,9 +0,0 @@
-const String baseurl = 'https://api.sportmonks.com/v3/football/';
-const String apiToken = 'api_token=ZrrkK1QZXgKPXmIO6mDHJ6CKg84jzuzp1VpaeKJmDxy4rah4dDXWXunvcBZ3';
-const String apiHeaders = 'Content-Type=application/json&Accept=application/json&';
-
-
-
-const String standingEndPoint = 'standings?';
-
-
