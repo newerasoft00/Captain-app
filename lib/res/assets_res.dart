82e663d32db0ac91f01d4e39bf277a30fd5bf8bb
Yasser Elbana <74818740+yasser1248@users.noreply.github.com>
2023-09-21 17:07:46 +0300

    def bet

---
+++ lib/res/assets_res.dart
@@ -0,0 +1,12 @@
+/// Generated file. Do not edit.
+
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+class AssetsRes {
+  static const String PLUGIN_NAME = 'sportsbet';
+  static const String PLUGIN_VERSION = '1.0.0+1';
+  static const String 1584889 = 'assets/1584889.png';
+  static const String DEF_LOGO = 'assets/Image/def_logo.jpg';
+  static const String APPAINTER_THEME DARK = 'assets/appainter_theme dark.json';
+  static const String APPAINTER_THEME = 'assets/appainter_theme.json';
+}
